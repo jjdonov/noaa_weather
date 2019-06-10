120b580592178f34c5ca5cd25cf2aa5c6a4366a7
John Donovan <jjdonov@gmail.com>
2019-06-10 13:26:59 -0700

    Ties CLI + ( Serivice + Formatter ) together

--- lib/noaa_weather/cli.ex
+++ lib/noaa_weather/cli.ex
@@ -1,5 +1,11 @@
 defmodule NoaaWeather.CLI do
-  def main do
+  alias NoaaWeather.Service
+  alias NoaaWeather.TableFormatter, as: Formatter
+
+  def main(argv) do
+    argv
+    |> parse_args()
+    |> process()
   end
 
   def parse_args(argv) do
@@ -13,4 +19,24 @@ defmodule NoaaWeather.CLI do
   defp args_to_internal_representation({[help: true], _, _}), do: :help
   defp args_to_internal_representation({_, [station], _}), do: {station}
   defp args_to_internal_representation(_), do: :help
+
+  def process(:help) do
+    IO.puts("""
+    usage: noaa_weather <station_id>
+    """)
+  end
+
+  def process({station}) do
+    Service.fetch(station)
+    |> print_result()
+  end
+
+  def print_result({:error, _}) do
+    IO.puts("Something went wrong")
+  end
+
+  def print_result({:ok, result}) when is_list(result) do
+    Formatter.format(result)
+    |> IO.puts()
+  end
 end
